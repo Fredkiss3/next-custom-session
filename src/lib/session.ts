d8739fc5d646cbd1cd6a0db723415ecb8c3533e2
Adrien KISSIE <fredkiss3@gmail.com>
2023-08-13 02:39:19 +0200

    🚧 create serialized session object

---
+++ src/lib/session.ts
@@ -0,0 +1,25 @@
+import { preprocess, z } from "zod";
+
+const sessionSchema = z.object({
+  id: z.string(),
+  signature: z.string(),
+  expiry: preprocess((arg) => new Date(arg as any), z.date()),
+  isBot: z.boolean().optional().default(false),
+  user: z
+    .object({
+      id: z.string(),
+    })
+    .nullish(),
+  flashMessages: z.record(z.enum(["success", "error"]), z.string()).optional(),
+  formData: z
+    .object({
+      data: z.record(z.string(), z.any()).nullish(),
+      errors: z.record(z.string(), z.array(z.string())).nullish(),
+    })
+    .nullish(),
+  extras: z.record(z.string(), z.any()).nullish(),
+});
+
+export type SerializedSession = z.TypeOf<typeof sessionSchema>;
+
+export class Session {}
